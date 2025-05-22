cebec9662721b5d75386a4208836265225ffae83
artem.prokhorov <artem.prokhorov@iteco.com>
2025-05-22 16:56:38 +0300

    Interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"fyne.io/fyne/v2/app"
 )
 
 const Version = "0.0.1"
 
 func main() {
-	fmt.Println("Hello, world")
+	app := app.New()
+	window := app.NewWindow("Huy")
+
+	window.ShowAndRun()
 }
